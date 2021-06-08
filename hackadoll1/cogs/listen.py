af7e129956b0f510358043391639a02bc0ca242e
rli99 <rogerli99999@gmail.com>
2021-06-09 01:05:17 +1000

    Automatically ban users with certain name patterns.

--- hackadoll1/cogs/listen.py
+++ hackadoll1/cogs/listen.py
@@ -1,3 +1,4 @@
+import discord
 import hkdhelper as hkd
 from discord import utils as disc_utils
 from discord.ext import commands
@@ -12,6 +13,17 @@ class Listen(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        if (guild := disc_utils.get(self.bot.guilds, id=hkd.SERVER_ID)) == member.guild:
+        if (guild := hkd.get_wug_guild()) == member.guild:
             channel = disc_utils.get(guild.channels, id=hkd.WELCOME_CHANNEL_ID)
-            await channel.send(embed=hkd.create_embed(title='{0} ({1}) has left the server.'.format(member.display_name, member)))
+            try:
+                await member.guild.fetch_ban(member)
+                await channel.send(embed=hkd.create_embed(title='{0} ({1}) has been banned from the server.'.format(member.display_name, member)))
+            except discord.NotFound:
+                await channel.send(embed=hkd.create_embed(title='{0} ({1}) has left the server.'.format(member.display_name, member)))
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.guild == hkd.get_wug_guild():
+            for pattern in hkd.BANNED_USER_PATTERNS:
+                if pattern in member.name:
+                    await member.ban()
